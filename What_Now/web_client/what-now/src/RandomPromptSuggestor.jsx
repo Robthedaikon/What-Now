567beab0ce269da5861029415e063475c8fdeaf2
Rob Paterson <robpaterson92@gmail.com>
2023-12-21 12:03:18 +0000

    build frontend React app to get random prompts

---
+++ What_Now/web_client/what-now/src/RandomPromptSuggestor.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import {fetchRandomPrompt, PromptType} from "./fetchRandomPrompt";
+
+export const RandomPromptSuggestor = () => {
+    const [prompt, setPrompt] = useState("");
+
+    const updatePrompt = async (promptType) => {
+        let suggestion = await fetchRandomPrompt(promptType);
+        while (suggestion.name === prompt) {
+                suggestion = await fetchRandomPrompt(promptType);
+            }
+        setPrompt(suggestion.name);
+    }
+
+    return (
+        <div className={"RandomPromptSuggestor"}>
+            <div>
+                <h2>Get a:</h2>
+                <div className={"RandomPromptSuggestorButtonsContainer"}>
+                    <button onClick={async () => updatePrompt(PromptType.CHARACTER)}>Character</button>
+                    <button onClick={async () => updatePrompt(PromptType.EMOTION)}>Emotion</button>
+                    <button onClick={async () => updatePrompt(PromptType.LINE)}>Line</button>
+                    <button onClick={async () => updatePrompt(PromptType.LOCATION)}>Location</button>
+                    <button onClick={async () => updatePrompt(PromptType.RELATIONSHIP)}>Relationship</button>
+                </div>
+            </div>
+            <h1>{prompt}</h1>
+        </div>
+    );
+}
---
+++ What_Now/web_client/what-now/src/fetchRandomPrompt.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'http://127.0.0.1:8000';
+
+export const PromptType = {
+    LINE: 'line',
+    CHARACTER: 'character',
+    LOCATION: 'location',
+    RELATIONSHIP: 'relationship',
+    EMOTION: 'emotion',
+}
+
+export const fetchRandomPrompt = async (promptType) => {
+    const response = await fetch(`${BASE_URL}/prompt/${promptType}/random/`,
+        {
+            method: 'GET',
+            headers: {'Accept': 'application/json'},
+        }
+    );
+    const prompt = await response.json();
+    return prompt;
+}
